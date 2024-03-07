eebadfe9cd532b06098eaaadec6741b6761b343c
Miftahurrofi Asid-Qi <miftahurasidqi@gmail.com>
2024-03-07 13:49:44 +0700

    add Portfolio

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -4,40 +4,65 @@
       <h1 class="text-2xl lg:text-3xl font-semibold">Latest Project</h1>
       <span class="w-40 h-[2px] absolute -bottom-2 bg-sky-700"></span>
     </div>
-    <div class="w-full grid grid-cols-1 gap-8 py-8 px-6 sm:px-0">
-      <!-- <div class="w-full grid grid-cols-1 lg:grid-cols-2 gap-8 text-center py-8 px-6 sm:px-0"> -->
-      <div class="grid grid-cols-1 lg:grid-cols-5 gap-3 lg:gap-5 shadow-md duration-500 p-4 rounded-lg bg-white hover:bg-opacity-10 bg-opacity-5">
-        <img class="lg:col-span-2 w-full rounded-lg lg:w-" src="../assets/images/Portfolio-HTML.png" alt="" />
-        <div class="flex flex-col justify-between lg:col-span-3 gap-6 lg:gap-0">
-          <div>
-            <p class="text-sky-600 -mb-1 lg:mb-1 lg:text-2xl">Personal Portfolio</p>
-            <p class="text-sky-600 text-opacity-80 lg:text-2xl">2022</p>
-            <p>Aplikasi Web untuk preensi dosen dan karyawan IBN Lampung dengan mengunakan QR Code. Aplikasi ini juga dapat melakukan pencatatan kehadiran dosen dan karyawan dengan cara scan QR code.</p>
-          </div>
-          <div class="flex gap-4">
-            <span class="text-xs lg:text-sm px-3 py-1 text-sky-100 rounded-full bg-sky-700 bg-opacity-30">HTML</span>
-            <span class="text-xs lg:text-sm px-3 py-1 text-sky-100 rounded-full bg-sky-700 bg-opacity-30">CSS</span>
-            <span class="text-xs lg:text-sm px-3 py-1 text-sky-100 rounded-full bg-sky-700 bg-opacity-30">JavaScript</span>
-          </div>
-        </div>
-      </div>
-      <div class="grid grid-cols-1 lg:grid-cols-5 gap-3 lg:gap-5 shadow-md duration-500 p-4 rounded-lg bg-white hover:bg-opacity-10 bg-opacity-5">
-        <img class="lg:col-span-2 w-full rounded-lg lg:w-" src="../assets/images/Presensi-QR.png" alt="" />
-        <div class="flex flex-col justify-between lg:col-span-3 gap-6 lg:gap-0">
-          <div>
-            <p class="text-sky-600 -mb-1 lg:mb-1 lg:text-2xl">Presensi QR Code IBN with MERN Stack</p>
-            <p class="text-sky-600 text-opacity-80 lg:text-2xl">2023</p>
-            <p>Aplikasi Web untuk preensi dosen dan karyawan IBN Lampung dengan mengunakan QR Code. Aplikasi ini juga dapat melakukan pencatatan kehadiran dosen dan karyawan dengan cara scan QR code.</p>
-          </div>
 
-          <div class="flex flex- gap-4">
-            <span class="text-xs lg:text-sm px-3 py-1 text-sky-100 rounded-full bg-sky-700 bg-opacity-30">React</span>
-            <span class="text-xs lg:text-sm px-3 py-1 text-sky-100 rounded-full bg-sky-700 bg-opacity-30">Express</span>
-            <span class="text-xs lg:text-sm px-3 py-1 text-sky-100 rounded-full bg-sky-700 bg-opacity-30">MongoDB</span>
-            <span class="text-xs lg:text-sm px-3 py-1 text-sky-100 rounded-full bg-sky-700 bg-opacity-30">TailwindCSS</span>
+    <div class="w-full grid grid-cols-1 gap-8 py-8 px-6 sm:px-0">
+      <a href="https://github.com/miftahurasidqi/final-project-team-3" target="_blank">
+        <div class="grid grid-cols-1 lg:grid-cols-5 gap-3 lg:gap-5 shadow-md duration-500 p-4 rounded-lg bg-white hover:bg-opacity-10 bg-opacity-5">
+          <img class="lg:col-span-2 w-full rounded-lg lg:w-" src="../assets/images/3Th-Party-Instruction.png" alt="" />
+          <div class="flex flex-col justify-between lg:col-span-3 gap-6 lg:gap-0">
+            <div>
+              <p class="text-sky-600 -mb-1 lg:mb-1 lg:text-2xl">3Th Party Instructions</p>
+              <p class="text-sky-600 text-opacity-80 lg:text-2xl">2023</p>
+              <p>Aplikasi Web pengelolaan barang, hasil dari Final Project Inosoft Boocamp</p>
+            </div>
+            <div class="flex gap-4">
+              <span class="text-xs lg:text-sm px-3 py-1 text-sky-100 rounded-full bg-sky-700 bg-opacity-30">VueJs</span>
+              <span class="text-xs lg:text-sm px-3 py-1 text-sky-100 rounded-full bg-sky-700 bg-opacity-30">Axios</span>
+              <span class="text-xs lg:text-sm px-3 py-1 text-sky-100 rounded-full bg-sky-700 bg-opacity-30">Laravel</span>
+              <span class="text-xs lg:text-sm px-3 py-1 text-sky-100 rounded-full bg-sky-700 bg-opacity-30">MongoDB</span>
+            </div>
           </div>
         </div>
-      </div>
+      </a>
+      <!--  -->
+      <a href="https://github.com/miftahurasidqi/QR-Code-presensi-dosen-karyawan-IBN" target="_blank">
+        <div class="grid grid-cols-1 lg:grid-cols-5 gap-3 lg:gap-5 shadow-md duration-500 p-4 rounded-lg bg-white hover:bg-opacity-10 bg-opacity-5">
+          <img class="lg:col-span-2 w-full rounded-lg lg:w-" src="../assets/images/Presensi-QR.png" alt="" />
+          <div class="flex flex-col justify-between lg:col-span-3 gap-6 lg:gap-0">
+            <div>
+              <p class="text-sky-600 -mb-1 lg:mb-1 lg:text-2xl">Presensi QR Code IBN with MERN Stack</p>
+              <p class="text-sky-600 text-opacity-80 lg:text-2xl">2023</p>
+              <p>Aplikasi Web untuk preensi dosen dan karyawan IBN Lampung dengan mengunakan QR Code. Aplikasi ini juga dapat melakukan pencatatan kehadiran dosen dan karyawan dengan cara scan QR code.</p>
+            </div>
+
+            <div class="flex flex- gap-4">
+              <span class="text-xs lg:text-sm px-3 py-1 text-sky-100 rounded-full bg-sky-700 bg-opacity-30">ReactJs</span>
+              <span class="text-xs lg:text-sm px-3 py-1 text-sky-100 rounded-full bg-sky-700 bg-opacity-30">ExpressJs</span>
+              <span class="text-xs lg:text-sm px-3 py-1 text-sky-100 rounded-full bg-sky-700 bg-opacity-30">MongoDB</span>
+              <span class="text-xs lg:text-sm px-3 py-1 text-sky-100 rounded-full bg-sky-700 bg-opacity-30">TailwindCSS</span>
+            </div>
+          </div>
+        </div>
+      </a>
+      <!--  -->
+      <a href="https://miftahurasidqi.github.io" target="_blank">
+        <div class="grid grid-cols-1 lg:grid-cols-5 gap-3 lg:gap-5 shadow-md duration-500 p-4 rounded-lg bg-white hover:bg-opacity-10 bg-opacity-5">
+          <img class="lg:col-span-2 w-full rounded-lg lg:w-" src="../assets/images/Portfolio-HTML.png" alt="" />
+          <div class="flex flex-col justify-between lg:col-span-3 gap-6 lg:gap-0">
+            <div>
+              <p class="text-sky-600 -mb-1 lg:mb-1 lg:text-2xl">Personal Portfolio</p>
+              <p class="text-sky-600 text-opacity-80 lg:text-2xl">2022</p>
+              <p>Web Personal Portfolio Pertama yang saya buat mengunakan HTML, CSS dan JavaScript.</p>
+            </div>
+            <div class="flex gap-4">
+              <span class="text-xs lg:text-sm px-3 py-1 text-sky-100 rounded-full bg-sky-700 bg-opacity-30">HTML</span>
+              <span class="text-xs lg:text-sm px-3 py-1 text-sky-100 rounded-full bg-sky-700 bg-opacity-30">CSS</span>
+
+              <span class="text-xs lg:text-sm px-3 py-1 text-sky-100 rounded-full bg-sky-700 bg-opacity-30">JavaScript</span>
+            </div>
+          </div>
+        </div>
+      </a>
     </div>
   </section>
 </template>
